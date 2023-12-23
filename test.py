11858d07ac3c0865bc93ba04783e4e5b26461adf
Adrian dos Anjos <adriandosanjos000@gmail.com>
2023-12-23 15:59:55 -0300

    update

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-import pyautogui
+print('START')
+
+import tkinter
+import pyautogui as gui
+import os
+
+if os.name == 'nt':
+    os.system("C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe")
+if os.name == 'posix':
+    os.system("firefox epicgames.com")
+
 
