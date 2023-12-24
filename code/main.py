a5bd839432c87b1b8255359cfa2ba5c258b2e6d9
Adrian dos Anjos <adriandosanjos000@gmail.com>
2023-12-24 17:18:43 -0300

    update

--- code/main.py
+++ code/main.py
@@ -1,4 +1,13 @@
 import tkinter
 import pyautogui as gui
 import setup
+import time
+import threading as thr
 
+t1=thr.Thread(setup.setup())
+t1.start()
+t1.join()
+
+time.sleep(10)
+
+print(gui.locateCenterOnScreen('./img/epicLogo.png'))
