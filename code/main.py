396fadebeb3d7a16a29cbe0c27905c10220f9bbd
Adrian dos Anjos <adriandosanjos000@gmail.com>
2023-12-24 18:11:39 -0300

    update

--- code/main.py
+++ code/main.py
@@ -3,6 +3,9 @@ import pyautogui as gui
 import setup
 import time
 
-#time.sleep(10)
+while not gui.pixelMatchesColor(35, 156, (255, 255, 255)):
+    time.sleep(1)
 
-print(gui.locateCenterOnScreen('./img/epicLogo.png'))
+while not gui.pixelMatchesColor(500, 1000, (0, 120, 242)):
+    gui.scroll(-1)
+    time.sleep(1)
