f89728666c5ece12d4049e1b1ff0df02f15ae788
Adrian dos Anjos <adriandosanjos000@gmail.com>
2023-12-24 17:28:10 -0300

    update

--- code/setup.py
+++ code/setup.py
@@ -1,7 +1,6 @@
 import os
 
-def setup():
-    if os.name == 'nt':
-        os.system("C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe")
-    if os.name == 'posix':
-        os.system("firefox epicgames.com/pt-BR")
+if os.name == 'nt':
+    os.system("C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe")
+if os.name == 'posix':
+    os.system("firefox epicgames.com/pt-BR")
