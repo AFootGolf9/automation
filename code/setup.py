f27e0b2ae9be4c81135b50175ed1e39e1fb5e05f
Adrian dos Anjos <adriandosanjos000@gmail.com>
2023-12-25 19:27:14 -0300

    update

--- code/setup.py
+++
@@ -1,6 +0,0 @@
-import os
-
-if os.name == 'nt':
-    os.system("C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe")
-if os.name == 'posix':
-    os.system("firefox epicgames.com/pt-BR &")
