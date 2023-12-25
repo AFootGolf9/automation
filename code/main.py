9f1f2350907f816fea5da36a0de5bf7add2febc3
Adrian dos Anjos <adriandosanjos000@gmail.com>
2023-12-25 13:11:47 -0300

    update

--- code/main.py
+++ code/main.py
@@ -6,6 +6,19 @@ import time
 while not gui.pixelMatchesColor(35, 156, (255, 255, 255)):
     time.sleep(1)
 
-while not gui.pixelMatchesColor(500, 1000, (0, 120, 242)):
-    gui.scroll(-1)
-    time.sleep(1)
+found = False
+
+while not found:
+    gui.scroll(-10)
+    img = gui.screenshot()
+    for i in range(0, 1080, 10):
+        color = img.getpixel((500, i))
+        if color == (0, 120, 242, 255):
+            found = True
+            break
+
+
+
+#while not gui.pixelMatchesColor(500, 1000, (0, 120, 242)):
+#    gui.scroll(-1)
+#    time.sleep(1)
