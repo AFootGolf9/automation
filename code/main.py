6bc2c00e88d87e4ef5be6882520a7b9b4cb6fd7c
Adrian dos Anjos <adriandosanjos000@gmail.com>
2023-12-29 19:15:08 -0300

    update

--- code/main.py
+++ code/main.py
@@ -1,57 +1,18 @@
-import requests
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import tkinter
+import pyautogui as gui
+import setup
 import time
 
+while not gui.pixelMatchesColor(35, 156, (255, 255, 255)):
+    time.sleep(1)
 
-def get_free_game():
-    # Make a request to the Epic Games API to get the free game information
-    response = requests.get("https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions")
+found = False
 
-    # Check the response status code to determine if the request was successful
-    if response.status_code == 200:
-        game_info = response.json()
-        game_name= game_info['data']['Catalog']['searchStore']['elements'][0]['productSlug']
-        return game_name
-    else:
-        print("Failed to retrieve the free game information.")
-
-freeGame = get_free_game()
-
-# Initialize the Chrome driver
-driver = webdriver.Chrome()
-
-# Navigate to the Epic Games Store
-driver.get("https://www.epicgames.com/store/en-US/")
-
-# Log in to your account
-# Note: You'll need to handle the login process here
-
-# Navigate to the game page
-driver.get("https://www.epicgames.com/store/en-US/p/" + freeGame)  # replace 'game-name' with the actual game name
-#time.sleep(200)
-
-time.sleep(10)
-while True:
-    try:
-        driver.refresh()
-        time.sleep(5)
-        driver.find_element("challenge-stage").click()
-        print("Captcha found")
-    except:
-        time.sleep(5)
-        print("No captcha") 
-        break    
-
-# Click on the 'Buy' button
-buy_button = driver.find_element("button.buy-button")
-buy_button.click()
-
-# Handle the checkout process
-# Note: You'll need to handle the checkout process here
-
-# Confirm the purchase
-# Note: You'll need to handle the purchase confirmation here
-
-# Close the browser
-driver.quit()
+while not found:
+    gui.scroll(-10)
+    img = gui.screenshot()
+    for i in range(0, 1080, 10):
+        color = img.getpixel((500, i))
+        if color == (0, 120, 242, 255):
+            found = True
+            break
